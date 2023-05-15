a5f540464e01441a631ef08255cf800fc6460803
FV <vecekspse@gmail.com>
2023-05-15 14:27:52 +0200

    reset

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
 "use strict"
 
-const seats = document.querySelectorAll(".seat");
-
-// for(let seat of seats) { ... }
-seats.forEach(seat => {
-    seat.addEventListener("click", function (evt) {    
-        seat.classList.toggle("bg-success");
-    });
-})
-
-const reset = document.querySelector(".reset");
-
-reset.addEventListener("click", function () {
-    document.querySelectorAll(".seats .seat.bg-success")
-    .forEach(seat => seat.classList.remove("bg-success"));
-});
-
 
